383bcc814b1c93ce647b0997fbdcf0bbba5d46ed
richiky <richiskyjuniorpaul@yahoo.com>
2023-12-10 04:59:39 -0800

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0:
-        last_digit = number % -(10)
-        print(-(last_digit), end='')
-    else:
-        last_digit = number % 10
-        print(last_digit, end='')
-    return abs(last_digit
+    last_digit = number % 10  # Using modulo (%) operator to get the last digit
+    print("The last digit of", number, "is:", last_digit)
+    return last_digit
+
+# Example usage:
+number = 12345
+last_digit = print_last_digit(number)
